91bc4531106f0acdf0d5378f01425a8e0ba5d544
Imam <imam0067@gmail.com>
2023-02-08 09:31:38 +0700

    refractor

--- sqlite_to_mysql.py
+++ sqlite_to_mysql.py
@@ -6,12 +6,14 @@ import os
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('--sqlite-db', required=True, help='Path to the SQLite database file')
-parser.add_argument('--mysql-user', required=True, help='MySQL username')
-parser.add_argument('--mysql-password', required=True, help='MySQL password')
-parser.add_argument('--mysql-host', required=True, help='MySQL host')
-parser.add_argument('--mysql-database', required=True, help='MySQL database name')
-parser.add_argument('--mysql-port', required=False, default='3306', help='MySQL port')
+parser.add_argument(
+    "--sqlite-db", required=True, help="Path to the SQLite database file"
+)
+parser.add_argument("--mysql-user", required=True, help="MySQL username")
+parser.add_argument("--mysql-password", required=True, help="MySQL password")
+parser.add_argument("--mysql-host", required=True, help="MySQL host")
+parser.add_argument("--mysql-database", required=True, help="MySQL database name")
+parser.add_argument("--mysql-port", required=False, default="3306", help="MySQL port")
 args = parser.parse_args()
 
 # check if sqlite file exists
@@ -20,17 +22,17 @@ if not os.path.exists(args.sqlite_db):
     exit(1)
 
 # Connect to the SQLite database
-engine = sa.create_engine(f'sqlite:///{args.sqlite_db}')
-
 # Create an inspector for the SQLite database
-inspector = sa.inspect(engine)
-
 # Get the list of tables in the SQLite database
+engine = sa.create_engine(f"sqlite:///{args.sqlite_db}")
+inspector = sa.inspect(engine)
 table_names = inspector.get_table_names()
 
 # Connect to the MySQL database using a SQLAlchemy engine
 try:
-    mysql_engine = sa.create_engine(f'mysql+mysqlconnector://{args.mysql_user}:{args.mysql_password}@{args.mysql_host}:{args.mysql_port}/{args.mysql_database}')
+    mysql_engine = sa.create_engine(
+        f"mysql+mysqlconnector://{args.mysql_user}:{args.mysql_password}@{args.mysql_host}:{args.mysql_port}/{args.mysql_database}"
+    )
 except sa.exc.OperationalError as e:
     if "Access denied for user" in str(e):
         print("Error: Invalid MySQL username or password.")
@@ -43,23 +45,21 @@ print("Successfully connected to MySQL database.")
 # Iterate through the tables in the SQLite database
 for table_name in table_names:
     # Drop the table if it already exists in the MySQL database
-    mysql_engine.execute(f'DROP TABLE IF EXISTS {table_name}')
+    mysql_engine.execute(f"DROP TABLE IF EXISTS {table_name}")
 
     # Load the data for the current table into a pandas DataFrame
-    df = pd.read_sql_table(table_name, engine)
-
     # Remove the "index" column from the DataFrame
-    df = df.drop(columns=['index'])
+    df = pd.read_sql_table(table_name, engine)
+    df = df.drop(columns=["index"])
 
     # Use pandas to generate the CREATE TABLE statement for the table
-    create_table_stmt = pd.io.sql.get_schema(df, table_name)
-
     # Escape the table and column names in the CREATE TABLE statement
-    create_table_stmt = re.sub('"([^"]+)"', r'`\1`', create_table_stmt)
+    create_table_stmt = pd.io.sql.get_schema(df, table_name)
+    create_table_stmt = re.sub('"([^"]+)"', r"`\1`", create_table_stmt)
 
     # Execute the CREATE TABLE statement
     mysql_engine.execute(create_table_stmt)
 
     # Write the data from the DataFrame to the MySQL table
-    df.to_sql(table_name, mysql_engine, if_exists='replace', index=False)
+    df.to_sql(table_name, mysql_engine, if_exists="replace", index=False)
     print(f"Table {table_name} inserted.")
